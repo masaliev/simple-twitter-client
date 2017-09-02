3cb736f25fcd48187b691db92483e88538bd8b98
Berdimurat Masaliev <muratmbt@gmail.com>
2017-09-02 17:33:03 +0600

    Login activity

---
+++ app/src/main/java/com/github/masaliev/simpletwitterclient/App.java
@@ -0,0 +1,30 @@
+package com.github.masaliev.simpletwitterclient;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+
+/**
+ * Created by mbt on 9/2/17.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(getString(R.string.com_twitter_sdk_android_CONSUMER_KEY),
+                        getString(R.string.com_twitter_sdk_android_CONSUMER_SECRET)))
+                .debug(true)
+                .build();
+
+        Twitter.initialize(config);
+    }
+}
--- app/src/main/java/com/github/masaliev/simpletwitterclient/LoginActivity.java
+++ app/src/main/java/com/github/masaliev/simpletwitterclient/LoginActivity.java
@@ -1,13 +1,63 @@
 package com.github.masaliev.simpletwitterclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TwitterLoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+        if(session != null) {
+            TwitterAuthToken authToken = session.getAuthToken();
+
+            if(authToken != null && !authToken.isExpired()){
+                goToMainActivity();
+                return;
+            }
+        }
+
         setContentView(R.layout.activity_login);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.btnTwitterLogin);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                goToMainActivity();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(LoginActivity.this, R.string.error_an_error_occurred_try_again, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goToMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        loginButton.onActivityResult(requestCode, resultCode, data);
     }
 }
